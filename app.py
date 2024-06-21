c2feabd35ac6ab3c10c10254757e8b71c0743931
Mukhammadjon <Mukhammadjon>
2024-06-22 03:56:57 +0500

    deploying production'

---
+++ app.py
@@ -0,0 +1,26 @@
+import logging
+import os
+from aiogram import Bot, Dispatcher, executor, types
+from dotenv import load_dotenv
+load_dotenv()
+
+API_TOKEN = os.getenv("API_TOKEN")
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    username = message.from_user.username
+    await message.reply(f"Salom @{username}")
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
