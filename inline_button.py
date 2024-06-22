67b477db6ed12dc8344e10a0970ea7a9f7f2ab31
Mukhammadjon <Mukhammadjon>
2024-06-22 05:18:46 +0500

    change

--- inline_button.py
+++ inline_button.py
@@ -1,4 +1,4 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-product_menu = InlineKeyboardMarkup()
+product_menu = InlineKeyboardMarkup(resize_keyboard=True)
 product_menu.add(InlineKeyboardButton(text="➖"), InlineKeyboardButton(text="count"), InlineKeyboardButton(text="➕"))
